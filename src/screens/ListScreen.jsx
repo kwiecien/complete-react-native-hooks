8bab3cde82cb79163a85e778ed11e85dcd92a4eb
kwiecien <k.d.kwiecinski@gmail.com>
2020-01-26 21:17:47 +0100

    add list screen

---
+++ src/screens/ListScreen.jsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text} from "react-native";
+import React from "react";
+
+const ListScreen = () => {
+    return <Text>
+        List Screen
+    </Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
