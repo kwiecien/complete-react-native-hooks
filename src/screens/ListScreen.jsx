b93cf3bbb6ce2f7ef64c2fa17b2648814e382cf0
kwiecien <k.d.kwiecinski@gmail.com>
2020-01-26 21:42:15 +0100

    show age

--- src/screens/ListScreen.jsx
+++ src/screens/ListScreen.jsx
@@ -3,23 +3,25 @@ import React from "react";
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1'},
-        {name: 'Friend #2'},
-        {name: 'Friend #3'},
-        {name: 'Friend #4'},
-        {name: 'Friend #5'},
-        {name: 'Friend #6'},
-        {name: 'Friend #7'},
-        {name: 'Friend #8'},
-        {name: 'Friend #9'},
+        {name: 'Friend #1', age: 20},
+        {name: 'Friend #2', age: 45},
+        {name: 'Friend #3', age: 32},
+        {name: 'Friend #4', age: 27},
+        {name: 'Friend #5', age: 53},
+        {name: 'Friend #6', age: 30},
+        {name: 'Friend #7', age: 33},
+        {name: 'Friend #8', age: 88},
+        {name: 'Friend #9', age: 99},
     ];
     return (
         <FlatList
             keyExtractor={friend => friend.name}
-            horizontal={false}
-            showsHorizontalScrollIndicator={false}
             data={friends}
-            renderItem={({item: friend}) => <Text style={styles.textStyle}>{friend.name}</Text>}>
+            renderItem={({item: friend}) => (
+                <Text style={styles.textStyle}>
+                    {friend.name} - Age {friend.age}
+                </Text>
+            )}>
             List Screen
         </FlatList>
     );
