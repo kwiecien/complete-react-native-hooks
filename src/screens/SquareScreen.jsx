b461026991b7cf1f2ea9d02be8e4859b818ae7fd
kwiecien <k.d.kwiecinski@gmail.com>
2020-02-08 16:58:04 +0100

    refactor

--- src/screens/SquareScreen.jsx
+++ src/screens/SquareScreen.jsx
@@ -7,23 +7,21 @@ const SquareScreen = () => {
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
 
+    const COLOR_INCREMENT = 30;
+
+    function getColorCounter(color, setColor, value) {
+        return <ColorCounter
+            color={color}
+            onIncrease={() => setColor(value + COLOR_INCREMENT)}
+            onDecrease={() => setColor(value - COLOR_INCREMENT)}
+        />;
+    }
+
     return <View>
-        <ColorCounter
-            color={'Red'}
-            onIncrease={() => setRed(red + 10)}
-            onDecrease={() => setRed(red - 10)}
-        />
-        <ColorCounter
-            color={'Green'}
-            onIncrease={() => setGreen(green + 10)}
-            onDecrease={() => setGreen(green - 10)}
-        />
-        <ColorCounter
-            color={'Blue'}
-            onIncrease={() => setBlue(blue + 10)}
-            onDecrease={() => setBlue(blue - 10)}
-        />
-        <View style={{height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
+        {getColorCounter('Red', setRed, red)}
+        {getColorCounter('Green', setGreen, green)}
+        {getColorCounter('Blue', setBlue, blue)}
+        <View style={{height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
     </View>
 };
 
