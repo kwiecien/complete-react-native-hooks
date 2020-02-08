ec78618b92a526f1820540cdd8638413ae25add5
kwiecien <k.d.kwiecinski@gmail.com>
2020-02-08 16:53:53 +0100

    color the square

--- src/screens/SquareScreen.jsx
+++ src/screens/SquareScreen.jsx
@@ -1,31 +1,30 @@
 import React, {useState} from 'react';
-import {Button, View, FlatList} from 'react-native';
+import {View} from 'react-native';
+import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
-    const [colors, setColors] = useState([]);
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
 
     return <View>
-        <Button
-            title={"Add a Color"}
-            onPress={() => {
-                setColors([...colors, randomRgb()]);
-            }}
+        <ColorCounter
+            color={'Red'}
+            onIncrease={() => setRed(red + 10)}
+            onDecrease={() => setRed(red - 10)}
         />
-        <FlatList
-            keyExtractor={(item) => item}
-            data={colors}
-            renderItem={({item}) => {
-                return <View style={{height: 100, width: 100, backgroundColor: item}}/>;
-            }}
+        <ColorCounter
+            color={'Green'}
+            onIncrease={() => setGreen(green + 10)}
+            onDecrease={() => setGreen(green - 10)}
         />
+        <ColorCounter
+            color={'Blue'}
+            onIncrease={() => setBlue(blue + 10)}
+            onDecrease={() => setBlue(blue - 10)}
+        />
+        <View style={{height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
     </View>
-};
-
-const randomRgb = () => {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    return `rgb(${red}, ${green}, ${blue})`;
 };
 
 export default SquareScreen;
