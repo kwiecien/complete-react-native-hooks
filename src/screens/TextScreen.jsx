1a37ec3fb2293b4f25156062175afcdfbcd30679
kwiecien <k.d.kwiecinski@gmail.com>
2020-03-03 22:29:47 +0100

    validate password length

--- src/screens/TextScreen.jsx
+++ src/screens/TextScreen.jsx
@@ -2,17 +2,17 @@ import React, {useState} from 'react';
 import {StyleSheet, TextInput, View, Text} from 'react-native';
 
 const TextScreen = () => {
-    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
     return <View>
-        <Text>Enter name:</Text>
+        <Text>Enter password:</Text>
         <TextInput
             style={styles.input}
             autoCapitalize={"none"}
             autoCorrect={false}
-            value={name}
-            onChangeText={value => setName(value)}
+            value={password}
+            onChangeText={value => setPassword(value)}
         />
-        <Text>My name is {name}</Text>
+        {password.length < 5 && <Text>Password must be at least 5 characters long!</Text>}
     </View>
 };
 
