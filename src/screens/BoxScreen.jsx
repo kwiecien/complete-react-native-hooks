6117173a53869f9b7dc75ad23e522e9892366629
kwiecien <k.d.kwiecinski@gmail.com>
2020-03-04 21:27:27 +0100

    box object model

--- src/screens/BoxScreen.jsx
+++ src/screens/BoxScreen.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 const BoxScreen = () => {
-return <View>
-    <Text>Box Screen</Text>
+return <View style={styles.viewStyle}>
+    <Text style={styles.textStyle}>Box Screen</Text>
 </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+    },
+    textStyle: {
+        borderWidth: 10,
+        borderColor: 'red',
+        margin: 20,
+    }
+});
 
 export default BoxScreen;
